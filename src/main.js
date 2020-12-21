8725bd192cc1903446c22d7da0362724dd13fa66
zihe <he100zihao100@126.com>
2020-12-21 12:37:22 +0800

    add: 处理全局样式覆盖问题

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 加载全局样式
-import './styles/index.less'
-
 // 引入Vant 组件库
 import Vant from 'vant'
 // 加载 Vant 组件库的样式
@@ -13,6 +10,9 @@ import 'vant/lib/index.css'
 
 // 自动设置 REM 基准值（html 标签的字体大小）
 import 'amfe-flexible'
+
+// 加载全局样式（最好放到最后，方便覆盖第三方样式）
+import './styles/index.less'
 
 // 全局注册 Vant 中的组件
 Vue.use(Vant)
