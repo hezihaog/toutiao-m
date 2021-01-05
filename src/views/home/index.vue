94a95b270f596d5328ec4f08e3eb60a50630fbbe
zihe <he100zihao100@126.com>
2021-01-05 11:45:25 +0800

    add: 添加首页Tab栏，显示频道列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -12,24 +12,49 @@
         搜索
       </van-button>
     </van-nav-bar>
+
+    <!-- 文章频道列表 -->
+    <van-tabs v-model="active">
+      <van-tab v-for="channel in channels"
+               :title="channel.name"
+               :key="channel.id">
+        {{ channel.name }} 的内容
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
+  import { getUserChannels } from '@/api/user'
+
   export default {
     name: 'HomeIndex',
     components: {},
     props: {},
     data () {
-      return {}
+      return {
+        //控制被激活的标签
+        active: 0,
+        //频道列表
+        channels: []
+      }
     },
     computed: {},
     watch: {},
     created () {
+      //加载文章标签列表
+      this.loadChannels()
     },
     mounted () {
     },
-    methods: {},
+    methods: {
+      async loadChannels () {
+        //请求获取频道数据
+        const { data } = await getUserChannels()
+        console.log(data)
+        this.channels = data.data.channels
+      }
+    },
   }
 </script>
 <style scoped lang="less">
@@ -43,9 +68,11 @@
       height: 32px;
       background: #5babfb;
       border: none;
+
       .van-icon {
         font-size: 16px;
       }
+
       .van-button__text {
         font-size: 14px;
       }
