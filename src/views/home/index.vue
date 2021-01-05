243cdcc89d8618225716a5712eec56e053b405e5
zihe <he100zihao100@126.com>
2021-01-05 11:33:15 +0800

    add: 添加首页搜索栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,54 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="app-nav-bar">
+      <van-button slot="title"
+                  class="search-btn"
+                  icon="search"
+                  type="info"
+                  round
+                  size="small">
+        搜索
+      </van-button>
+    </van-nav-bar>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "HomeIndex",
-  components: {},
-  props: {},
-  data() {
-    return {};
-  },
-  computed: {},
-  watch: {},
-  created() {},
-  mounted() {},
-  methods: {},
-};
+  export default {
+    name: 'HomeIndex',
+    components: {},
+    props: {},
+    data () {
+      return {}
+    },
+    computed: {},
+    watch: {},
+    created () {
+    },
+    mounted () {
+    },
+    methods: {},
+  }
 </script>
+<style scoped lang="less">
+  .home-container {
+    /deep/ .van-nav-bar__title {
+      max-width: none;
+    }
 
-<style scoped lang="less"></style>
+    .search-btn {
+      width: 277px;
+      height: 32px;
+      background: #5babfb;
+      border: none;
+      .van-icon {
+        font-size: 16px;
+      }
+      .van-button__text {
+        font-size: 14px;
+      }
+    }
+  }
+</style>
